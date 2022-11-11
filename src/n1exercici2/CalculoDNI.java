bc2c512b01cffb42107bedead51c37c487609827
tokyumilla <tokyumilla@gmail.com>
2022-11-11 17:46:42 +0100

    SWITCH O ARRAYS

---
+++ src/n1exercici2/CalculoDNI.java
@@ -0,0 +1,66 @@
+package n1exercici2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CalculoDNI {
+    private int number;
+    private char character;
+    private final List<char> characterList = new ArrayList<>(Arrays.asList('T', 'R' ))
+
+    public CalculoDNI(int number) {
+        this.number = number;
+        int module = number% 23;
+        switch (module) {
+            case 0:
+                this.character = 'R';
+                break;
+            case 0:
+                this.character = 'R';
+                break;
+            case 0:
+                this.character = 'R';
+                break;
+            case 0:
+                this.character = 'R';
+                break;
+            case 0:
+                this.character = 'R';
+                break;
+            case 0:
+                this.character = 'R';
+                break;
+            case 0:
+                this.character = 'R';
+                break;
+            case 0:
+                this.character = 'R';
+                break;
+            case 0:
+                this.character = 'R';
+                break;
+            case 0:
+                this.character = 'R';
+                break;
+            case 0:
+                this.character = 'R';
+                break;
+            case 0:
+                this.character = 'R';
+                break;
+            case 0:
+                this.character = 'R';
+                break;
+            case 0:
+                this.character = 'R';
+                break;
+            case 0:
+                this.character = 'R';
+                break;
+
+        }
+    }
+
+
+}
